bb645526cc46d0953fc84301aea1184c30b5d36f
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2017-02-24 22:12:22 +0200

    more solutions added

--- emacs-lisp/lists.el
+++ emacs-lisp/lists.el
@@ -49,3 +49,109 @@
    (t (if (listp (car seq))
           (append (my-flatten (car seq)) (my-flatten (cdr seq)))
         (append (list (car seq)) (my-flatten (cdr seq)))))))
+
+(defun compress (seq)
+  (if (< (my-length seq) 2)
+      seq
+    (let ((lst seq))
+      (if (eq (car lst) (element-at lst 2))
+          (compress (cdr lst))
+        (append (list (car lst)) (compress (cdr lst)))))))
+
+(defun pack (seq)
+  (if (null seq)
+      nil
+    (let ((item (car seq))
+          (res (list (car seq)))
+          (lst (cdr seq)))
+      (while (eq item (car lst))
+        (setq res (append res (list item)))
+        (setq lst (cdr lst)))
+      `(,res ,@(pack lst)))))
+
+(defun encode (seq)
+  (mapcar (lambda (lst)
+            (list (count (car lst) lst) (car lst)))
+          (pack seq)))
+
+(defun encode-modified (seq)
+  (mapcar (lambda (lst)
+            (cond ((eq (car lst) 1) (cadr lst))
+                  (t lst)))
+          (encode seq)))
+
+(defun decode (seq)
+  (let ((res '()))
+    (mapcar (lambda (lst)
+              (dotimes (i (car lst))
+                (setq res (append res (list (cadr lst))))))
+            seq)
+    res))
+
+(defun my-last (seq)
+  (car (last seq)))
+
+(defun my-head (seq)
+  (reverse (cdr (reverse seq))))
+
+(defun dupli (seq)
+  (my-flatten (mapcar (lambda (item) (list item item)) seq)))
+
+(defun repli (seq n)
+  (my-flatten
+   (mapcar (lambda (item)
+             (let ((res ()))
+               (dotimes (i n)
+                 (setq res (append res (list item))))
+               res))
+           seq)))
+
+(defun drop-every (seq n)
+  (let ((i 1)
+        (res ()))
+    (dotimes (i (my-length seq))
+      (setq i (1+ i))
+      (if (eq (mod i n) 0)
+          nil
+        (setq res (append res (list (element-at seq i))))))
+    res))
+
+(defun take (seq n)
+  (if (>= n (my-length seq))
+      seq
+    (let ((res ()))
+      (dotimes (i n res)
+        (setq res (append res (list (element-at seq (1+ i)))))))))
+
+(defun drop (seq n)
+  (reverse (take (reverse seq) (- (my-length seq) n))))
+
+(defun split (seq n)
+  (if (>= n (my-length seq))
+      `(,seq ())
+    `(,(take seq n) ,(drop seq n))))
+
+(defun slice (seq start stop)
+  (take (drop seq start) (- stop start)))
+
+(defun rotate (seq n)
+  (append
+   (drop seq n)
+   (take seq n)))
+
+(defun remove-at (seq pos)
+  (append
+   (take seq (1- pos))
+   (drop seq pos)))
+
+(defun insert-at (item seq pos)
+  (append
+   (take seq (1- pos))
+   (list item)
+   (drop seq pos)))
+
+(defun range (start stop)
+  (if (> start stop) (error "Start should be < than stop"))
+  (let ((res ()))
+    (dotimes (i (- (1+ stop) start) res)
+      (setq res (append res (list (+ i start)))))))
